f16a5127d590373739a69d84da2b93e2199b61d7
Fumihiro Ito <fmhrit@gmail.com>
2013-10-30 14:13:57 +0900

    support fedora 19

--- site-cookbooks/mysql/attributes/server.rb
+++ site-cookbooks/mysql/attributes/server.rb
@@ -2,7 +2,7 @@ default['mysql']['server']['id'] = 1
 
 case node['platform_family']
 when 'fedora'
-  default['mysql']['server']['packages'] = %w[mysql-server]
+  default['mysql']['server']['packages'] = node['platform_version'].to_i >= 19 ? %w[community-mysql-server] : %w[mysql-server]
 else
   default['mysql']['server']['packages'] = %w[mysql-server]
 end
