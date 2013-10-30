f64b254d980da58ea216b46b9ce06ab57d35c336
Fumihiro Ito <fmhrit@gmail.com>
2013-10-30 14:14:40 +0900

    remove supervisord cookbook

--- site-cookbooks/supervisord/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'supervisord'
-maintainer       'Fumihiro Ito'
-maintainer_email 'fmhrit@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures supervisord'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
-supports 'fedora'
